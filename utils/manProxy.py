b52c0a0509d0c5b1416352736646bc112e3f7542
Caesium <caesium133@163.com>
2022-04-06 16:03:26 +0800

    改用proxy

--- utils/manProxy.py
+++ utils/manProxy.py
@@ -12,48 +12,48 @@ def get_proxy():
 def delete_proxy(proxy):
     requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
 
-# def getHtml(url):
-#     retry_count = utils.gParas.retry_times
-#     proxy = get_proxy().get("proxy")
-#     while retry_count > 0:
-#         try:
-#             html = requests.get(url, proxies={"http": "http://{}".format(proxy), "https": "https://{}".format(proxy)},headers=utils.gParas.headers,timeout=5)
-#             if html is None:
-#                 raise NoRespondException
-#             else:
-#                 return html
-#         except:
-#             retry_count -= 1
-#             delete_proxy(proxy)
-#             proxy = get_proxy().get("proxy")
-#     if retry_count==0:
-#         logging.warning("no proxy valid")
-#     return requests.get(url)
-#
-#
-# def postHtml(url, data, headers=None):
-#     retry_count = utils.gParas.retry_times
-#     proxy = get_proxy().get("proxy")
-#     while retry_count > 0:
-#         try:
-#             html = requests.post(url=url, data=data, headers=headers, proxies={"http": "http://{}".format(proxy),"https": "https://{}".format(proxy)},
-#             timeout=5)
-#             if html is None:
-#                 raise NoRespondException
-#             else:
-#                 return html
-#         except:
-#             retry_count -= 1
-#             delete_proxy(proxy)
-#             proxy = get_proxy().get("proxy")
-#     if retry_count==0:
-#         logging.warning("no proxy valid")
-#     return requests.post(url=url, data=data, headers=headers)
-
-
 def getHtml(url):
-    return requests.get(url, headers=utils.gParas.headers, timeout=5)
+    retry_count = utils.gParas.retry_times
+    proxy = get_proxy().get("proxy")
+    while retry_count > 0:
+        try:
+            html = requests.get(url, proxies={"http": "http://{}".format(proxy), "https": "https://{}".format(proxy)},headers=utils.gParas.headers,timeout=5)
+            if html is None:
+                raise NoRespondException
+            else:
+                return html
+        except:
+            retry_count -= 1
+            delete_proxy(proxy)
+            proxy = get_proxy().get("proxy")
+    if retry_count==0:
+        logging.warning("no proxy valid")
+    return requests.get(url)
 
 
 def postHtml(url, data, headers=None):
-    return requests.post(url=url, data=data, headers=headers, timeout=5)
+    retry_count = utils.gParas.retry_times
+    proxy = get_proxy().get("proxy")
+    while retry_count > 0:
+        try:
+            html = requests.post(url=url, data=data, headers=headers, proxies={"http": "http://{}".format(proxy),"https": "https://{}".format(proxy)},
+            timeout=5)
+            if html is None:
+                raise NoRespondException
+            else:
+                return html
+        except:
+            retry_count -= 1
+            delete_proxy(proxy)
+            proxy = get_proxy().get("proxy")
+    if retry_count==0:
+        logging.warning("no proxy valid")
+    return requests.post(url=url, data=data, headers=headers)
+
+
+# def getHtml(url):
+#     return requests.get(url, headers=utils.gParas.headers, timeout=5)
+#
+#
+# def postHtml(url, data, headers=None):
+#     return requests.post(url=url, data=data, headers=headers, timeout=5)
